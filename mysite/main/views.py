dae52c9b2ae8b06320100c3d5846dbc6f5643743
@shubham860 <shubham.chauhan860@gmail.com>
2019-04-12 01:49:14 +0530

    update

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tutorial
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse("<h1 style='background-color:black;color:white;'>First Django application</h1>")
+    return render(request = request,
+                  template_name='main/home.html',
+                  context = {"tutorials":Tutorial.objects.all})
