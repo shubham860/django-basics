33556dbee74a8cf3430e6106b41c4aa13c066b71
@shubham860 <shubham.chauhan860@gmail.com>
2019-04-15 14:20:33 +0530

    update

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -20,5 +20,6 @@ from . import views
 app_name = 'main'  # here for namespacing of urls.
 
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
+  path("", views.homepage, name="homepage"),
+  path("register/", views.register, name="register"),
 ]
