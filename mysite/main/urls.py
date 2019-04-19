47ab4eb926c72cb09b85ce131e862834701a807c
@shubham860 <shubham.chauhan860@gmail.com>
2019-04-19 16:21:18 +0530

    update

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -22,4 +22,6 @@ app_name = 'main'  # here for namespacing of urls.
 urlpatterns = [
   path("", views.homepage, name="homepage"),
   path("register/", views.register, name="register"),
+  path("logout/", views.logout_request, name="logout"),
+  path("login/", views.login_request, name="login"),
 ]
