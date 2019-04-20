e3b1576c90cbf04a334331ef294eb9a47afe6854
@shubham860 <shubham.chauhan860@gmail.com>
2019-04-20 21:46:37 +0530

    update

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -31,7 +31,7 @@ class TutorialSeries(models.Model):
 class Tutorial(models.Model):
     tutorial_title = models.CharField(max_length=200)
     tutorial_content = models.TextField()
-    tutorial_published = models.DateTimeField('date published')
+    tutorial_published = models.DateTimeField('date published',default=datetime.now())
     #https://docs.djangoproject.com/en/2.1/ref/models/fields/#django.db.models.ForeignKey.on_delete
     tutorial_series = models.ForeignKey(TutorialSeries, default=1, verbose_name="Series", on_delete=models.SET_DEFAULT)
     tutorial_slug = models.CharField(max_length=200, default=1)
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,15 +1,39 @@
 from django.shortcuts import render, redirect
-from .models import Tutorial
+from .models import Tutorial, TutorialSeries, TutorialCategory
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import logout, authenticate, login
 from django.contrib import messages
 from .forms import NewUserForm
+from django.http import HttpResponse
 
-# Create your views here.
+
+def single_slug(request, single_slug):
+    # first check to see if the url is in categories.
+
+    categories = [c.category_slug for c in TutorialCategory.objects.all()]
+    if single_slug in categories:
+        matching_series = TutorialSeries.objects.filter(tutorial_category__category_slug=single_slug)
+        series_urls = {}
+
+        for m in matching_series.all():
+            part_one = Tutorial.objects.filter(tutorial_series__tutorial_series=m.tutorial_series).earliest("tutorial_published")
+            series_urls[m] = part_one.tutorial_slug
+
+        return render(request=request,
+                      template_name='main/category.html',
+                      context={"tutorial_series": matching_series, "part_ones": series_urls})
+
+
+    tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
+    if single_slug in tutorials:
+      return HttpResponse(f"{single_slug} is a Tutorial")
+
+    return HttpResponse(f"'{single_slug}' does not correspond to anything we know of!")
+
 def homepage(request):
-    return render(request = request,
-                  template_name='main/home.html',
-                  context = {"tutorials":Tutorial.objects.all})
+    return render(request=request,
+                  template_name='main/categories.html',
+                  context={"categories": TutorialCategory.objects.all})
 
 def register(request):
     if request.method == "POST":
